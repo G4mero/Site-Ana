fe289c88520ffc7bd7cf23d914c6159a3bd65323
G4mero <rafasinacio@gmail.com>
2024-06-07 15:50:02 -0300

    a

--- script.js
+++ script.js
@@ -34,9 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
             "Ana, você ilumina meus dias!",
             "Ana, amo você mais a cada dia!",
             "Ana, você é incrível!",
-            "Ana, seu sorriso ilumina meu dia!"
-        
-
             // Adicione mais mensagens aqui, total de 50
         ];
         mensagemContainer.textContent = mensagens[index];
@@ -45,15 +42,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // Adicionar evento de clique a todas as cartinhas
     cartinhas.forEach(cartinha => {
         cartinha.addEventListener('click', function() {
-            if (cartinhasAbertasHoje() < 5) {
-                const index = this.getAttribute('data-index');
-                exibirMensagem(index);
-                incrementarCartinhasAbertas();
-                this.style.pointerEvents = 'none'; // Desabilitar clique na cartinha já aberta
-                this.style.opacity = '0.5'; // Visualizar que a cartinha foi aberta
-            } else {
-                mensagemContainer.textContent = "Ana, você já abriu 5 cartinhas hoje! Volte amanhã para mais surpresas!";
-            }
+            const index = this.getAttribute('data-index');
+            exibirMensagem(index);
+            incrementarCartinhasAbertas();
         });
     });
 });
